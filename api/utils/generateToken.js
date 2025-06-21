8756547432101462190406845519bd849191f9b6
amitchahar9837 <amitchahar9837@gmail.com>
2025-06-21 17:26:54 +0530

    debugging to fis issue

--- api/utils/generateToken.js
+++ api/utils/generateToken.js
@@ -5,5 +5,5 @@ dotenv.config();
 
 export const generateToken = (id, res) =>{
     const token = jwt.sign({id}, process.env.JWT_KEY, {expiresIn:'1d'})
-    res.cookie('token', token, {httpOnly: true, maxAge: 24 * 60 * 60 * 1000, secure: process.env.NODE_ENV === 'production'});
+    res.cookie('token', token, {httpOnly: true, sameSite:"None", maxAge: 24 * 60 * 60 * 1000, secure: process.env.NODE_ENV === 'production'});
 }
