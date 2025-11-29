cf718280ae866392f9a44c3ad465886eb002998a
amitchahar9837 <amitchahar9837@gmail.com>
2025-11-29 17:25:45 +0100

    make it like before because of some need

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,10 @@
-import React from 'react';
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
   plugins: [
-    React(), 
+    react(), 
     VitePWA({
       registerType: "autoUpdate",
       includeAssets: ["favicon.ico", "apple-touch-icon.png", "robots.txt"],
