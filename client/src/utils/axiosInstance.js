f1c5afd40da3c506ae23496b7cd87704bc369ede
amitchahar9837 <amitchahar9837@gmail.com>
2026-02-28 13:41:56 +0530

    updated axiosinstance to fix issue

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 // const baseURL= import.meta.env.VITE_BASE_URL;
 export const axiosInstance = axios.create({
-  baseURL:`http://localhost:3001/api`,
+  baseURL:`/api`,
   withCredentials: true
 });
 
